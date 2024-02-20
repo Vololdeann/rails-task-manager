47033028c065fd14f67c06750977a96503e4aa0a
Olivier Volpi <olivolpi@gmail.com>
2024-02-20 16:45:14 +0100

    tasks

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,13 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "tasks#index"
+  get "tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new"
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  get "tasks/:id", to: "tasks#show", as: :task
+  post "tasks", to: "tasks#create"
+  patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy"
+
 end
